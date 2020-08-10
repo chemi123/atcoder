9da32a798468a78475040a663dbf575d92464315
Yuichi Tanimoto <>
2020-08-10 15:17:40 +0900

    half_and_half.cpp

---
+++ full_search_practice/half_and_half.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  uint64_t a, b, c, x, y;
+  cin >> a >> b >> c >> x >> y;
+  uint64_t res = 10e15;
+  res = min(res, a * x + b * y);
+  if (x > y) {
+    res = min(res, 2 * c * x);
+    res = min(res, 2 * c * y + a * (x - y));
+  } else {
+    res = min(res, 2 * c * y);
+    res = min(res, 2 * c * x + b * (y - x));
+  }
+  cout << res << endl;
+  return 0;
+}
