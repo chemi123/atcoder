81a840dd35fbee9d2c471ff45613fd3e784575d0
Yuichi Tanimoto <>
2020-08-13 08:29:05 +0900

    bit search

--- full_search_practice/exhaustive_search.cpp
+++ full_search_practice/exhaustive_search.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_A&lang=ja
+
+// dfsによる全探索でも良いが、bit探索の練習なので今回は使わない
 bool search(const vector<int>& a, int m, int index, int sum) {
   if (sum == m) return true;
   if (index == (int)a.size()) return false;
@@ -10,6 +13,20 @@ bool search(const vector<int>& a, int m, int index, int sum) {
   bool res = search(a, m, index + 1, sum + a[index]);
   res |= search(a, m, index + 1, sum);
   return res;
+}
+
+bool bitSearch(const vector<int>& a, int m) {
+  int n = (int)a.size();
+  for (int bit = 0; bit < (1 << n); ++bit) {
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+      if (bit & (1 << i)) {
+        sum += a[i];
+      }
+    }
+    if (sum == m) return true;
+  }
+  return false;
 }
 
 int main() {
@@ -22,7 +39,7 @@ int main() {
   for (int i = 0; i < q; ++i) {
     int m;
     cin >> m;
-    if (search(a, m, 0, 0)) {
+    if (bitSearch(a, m)) {
       cout << "yes" << endl;
     } else {
       cout << "no" << endl;
