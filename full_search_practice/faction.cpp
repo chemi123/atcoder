a84717cdc4269ba42c1c034b6e5e2f4ae2c65112
Yuichi Tanimoto <>
2020-08-14 23:19:49 +0900

    faction.cpp

---
+++ full_search_practice/faction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// https://atcoder.jp/contests/abc002/tasks/abc002_4
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<bool>> combi(13, vector(13, false));
+  for (int i = 0; i < m; ++i) {
+    int x, y;
+    cin >> x >> y;
+    combi[x][y] = true;
+  }
+  int ans = 1;
+  for (int bit = 0; bit < (1 << n); ++bit) {
+    vector<int> vec;
+    for (int i = 0; i < n; ++i) {
+      if (bit & (1 << i)) vec.emplace_back(i+1);
+    }
+    bool ok = true;
+    for (int i = 0; i < (int)vec.size()-1; ++i) {
+      for (int j = i + 1; j < (int)vec.size(); ++j) {
+        if (!combi[vec[i]][vec[j]]) ok = false;
+      }
+    }
+    if (ok) ans = max(ans, (int)vec.size());
+  }
+  cout << ans << endl;
+  return 0;
+}
