50bfe7833d453c2644ad00e01316af2f22ecbbc6
Yuichi Tanimoto <>
2020-08-15 14:46:28 +0900

    fix

--- template.cpp
+++ template.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
 
 using namespace std;
+using ul = uint64_t;
 
 int main() {
   return 0;
