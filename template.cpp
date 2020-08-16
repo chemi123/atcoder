2a9a1d60c382694711bbdbf46cd67868ee2a0bd5
Yuichi Tanimoto <>
2020-08-16 09:36:56 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -3,6 +3,10 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <bitset>
+#include <algorithm>
+#include <cmath>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
 
 using namespace std;
 using ul = uint64_t;
