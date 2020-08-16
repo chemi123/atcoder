693b7ab8bcc23f25d414583117c006f000d83f83
Yuichi Tanimoto <>
2020-08-17 08:28:18 +0900

    fix template

--- template.cpp
+++ template.cpp
@@ -1,10 +1,12 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
-#include <vector>
+#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
-#include <bitset>
-#include <algorithm>
-#include <cmath>
+#include <vector>
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #define debug(s, param) std::cerr << s << param << std::endl;
