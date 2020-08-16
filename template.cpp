8489de1111c26bf3c7bc1d7f661e9680eb8868ee
Yuichi Tanimoto <>
2020-08-16 11:49:00 +0900

    fix template

--- template.cpp
+++ template.cpp
@@ -7,12 +7,10 @@
 #include <cmath>
 
 using namespace std;
+using ll = long long;
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #define debug(s, param) cerr << s << param << endl;
-
-using ul = uint64_t;
-using ll = long long;
 
 int main() {
   int n = 10;
