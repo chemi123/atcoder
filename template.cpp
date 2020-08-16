f89ba70eada23fe2a86cec0de9f95841afae015d
Yuichi Tanimoto <>
2020-08-16 09:43:34 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -6,12 +6,16 @@
 #include <algorithm>
 #include <cmath>
 
-#define rep(i, n) for (int i = 0; i < n; ++i)
-
 using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define debug(s, param) cerr << s << param << endl;
+
 using ul = uint64_t;
 using ll = long long;
 
 int main() {
+  int n = 10;
+  debug("param: ", n);
   return 0;
 }
