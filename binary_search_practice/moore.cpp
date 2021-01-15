62d49dd6b63fc1de7e48edb4ea3f06a76911b908
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-01-15 09:33:48 +0900

    fix moore.cpp

--- binary_search_practice/moore.cpp
+++ binary_search_practice/moore.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -14,28 +15,20 @@
 using namespace std;
 using ll = long long;
 
-// https://atcoder.jp/contests/arc054/tasks/arc054_b
-
-double f(double x, double p) {
-  return x + p / pow(2, x / 1.5);
-}
+const ll MOD = 1000000007;
 
 int main() {
   double p;
   cin >> p;
   double l = 0, r = 1e15;
   int cnt = 1000;
+  auto f = [p](double x) { return x + p / pow(2, x / 1.5); };
   while (cnt--) {
-    double x1 = (l * 2 + r) / 3;
-    double x2 = (l + r * 2) / 3;
-    double f1 = f(x1, p);
-    double f2 = f(x2, p);
-    if (f1 > f2) {
-      l = x1;
-    } else {
-      r = x2;
-    }
+    double x1 = (l * 2 + r) / 3, x2 = (l + r * 2) / 3;
+    auto fx1 = f(x1), fx2 = f(x2);
+    if (fx1 > fx2) l = x1;
+    else r = x2;
   }
-  cout << fixed << setprecision(20) << f(r, p) << endl;
+  cout << fixed << setprecision(20) << f(r) << endl;
   return 0;
 }
