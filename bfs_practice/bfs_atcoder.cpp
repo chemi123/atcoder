4629158c7a25ec4bc046b5b4110f890c00a15122
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-04-24 06:35:30 +0900

    update bfs_adcoder simpler way

--- bfs_practice/bfs_atcoder.cpp
+++ bfs_practice/bfs_atcoder.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <set>
 #include <sstream>
 #include <queue>
 #include <unordered_map>
@@ -10,55 +11,44 @@
 #include <vector>
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
 #define debug(s, param) std::cerr << s << param << std::endl;
 
 using namespace std;
 using ll = long long;
+using pi = pair<int, int>;
+
+const int INF = 1e9;
+const ll INFL = 1e15;
+const ll MOD = 1000000007;
 
 // https://atcoder.jp/contests/abc007/tasks/abc007_3
-
-bool available(const vector<string>& grids, int r, int c) {
-  if (r < 0 || r >= (int)grids.size() || c < 0 || c >= (int)grids[0].size() || grids[r][c] == '#') return false;
-  return true;
-}
 
 int main() {
   int r, c;
   cin >> r >> c;
-  int sr, sc;
-  cin >> sr >> sc;
-  --sr, --sc;
-  int gr, gc;
-  cin >> gr >> gc;
-  --gr, --gc;
-  vector<string> grids(r, "");
-  rep (i, r) cin >> grids[i];
-
-  queue<pair<int, int>> q;
-  vector<vector<int>> dxdy{vector<int>{-1, 0}, vector<int>{1, 0}, vector<int>{0, -1}, vector<int>{0, 1}};
-  vector<vector<bool>> visit(r, vector<bool>(c, false));
-  int step = 0;
-
-  q.emplace(sr, sc);
-  visit[sr][sc] = true;
-  while (!q.empty()) {
-    int size = (int)q.size();
-    for (int i = 0; i < size; ++i) {
-      int row = q.front().first, col = q.front().second;
-      q.pop();
-      if (row == gr && col == gc) {
-        cout << step << endl;
-        return 0;
-      }
-      for (auto& v : dxdy) {
-        if (available(grids, row + v[0], col + v[1]) && !visit[row + v[0]][col + v[1]]) {
-          visit[row + v[0]][col + v[1]] = true;
-          q.emplace(row + v[0], col + v[1]);
-        }
-      }
+  int sr, sc, gr, gc;
+  cin >> sr >> sc >> gr >> gc;
+  --sr, --sc, --gr, --gc;
+  vector<string> graph(r);
+  rep (i, r) cin >> graph[i];
+  vector<pi> drdc{pi(1, 0), pi(-1, 0), pi(0, 1), pi(0, -1)};
+  vector<vector<int>> dist(r, vector<int>(c, -1));
+  dist[sr][sc] = 0;
+  queue<pi> que;
+  que.emplace(pi(sr, sc));
+  while (!que.empty()) {
+    auto [row, col] = que.front();
+    que.pop();
+    for (auto [dr, dc] : drdc) {
+      int nextRow = row + dr, nextCol = col + dc;
+      if (nextRow < 0 || nextRow >= r || nextCol < 0 || nextCol >= c) continue;
+      if (graph[nextRow][nextCol] == '#') continue;
+      if (dist[nextRow][nextCol] != -1) continue;
+      dist[nextRow][nextCol] = dist[row][col] + 1;
+      que.emplace(pi(nextRow, nextCol));
     }
-    ++step;
   }
-  cout << -1 << endl;
+  cout << dist[gr][gc] << endl;
   return 0;
 }
