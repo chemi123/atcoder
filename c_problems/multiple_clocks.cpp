41600d846ec8b806a06f1a86ec92d9a6a7e90e45
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-02 18:24:49 +0900

    multiple_clocks.cpp

---
+++ c_problems/multiple_clocks.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc070/tasks/abc070_c
+
+int main() {
+  int n; cin >> n;
+  ll ans = 1;
+  rep (i, n) {
+    ll num; cin >> num;
+    ans = lcm(ans, num);
+  }
+  cout << ans << endl;
+  return 0;
+}
