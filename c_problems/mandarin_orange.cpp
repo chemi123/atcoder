b7f0bb03692b0bc38ce658686cdd8712ccc3a1c2
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-11 07:54:23 +0900

    mandarin_orange.cpp

---
+++ c_problems/mandarin_orange.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc189/tasks/abc189_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n), indices;
+  rep (i, n) {
+    cin >> a[i];
+    if (i == 0 || a[i] != a[i-1]) indices.emplace_back(i);
+  }
+  int ans = 0;
+  for (auto l : indices) {
+    int sum = 0, x = a[l];
+    reps (r, l, n) {
+      x = min(x, a[r]);
+      sum = (r - l + 1) * x;
+      ans = max(ans, sum);
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
