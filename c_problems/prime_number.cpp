2ecc3ed0227389b3445cf8575d66e2b4b1dc76d6
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-13 09:17:20 +0900

    prime_number.cpp

---
+++ c_problems/prime_number.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/arc017/tasks/arc017_1
+
+int main() {
+  int n; cin >> n;
+  for (int i = 2; i * i <= n; ++i) {
+    if (n % i == 0) {
+      cout << "NO" << endl;
+      return 0;
+    }
+  }
+  cout << "YES" << endl;
+  return 0;
+}
+
