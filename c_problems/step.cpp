d2ed1bcb636bb242d25182f68b316576410eb075
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-16 09:51:12 +0900

    step.cpp

---
+++ c_problems/step.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc176/tasks/abc176_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  rep (i, n) cin >> a[i];
+  ll ans = 0;
+  reps (i, 1, n) {
+    int diff = max(0, a[i-1] - a[i]);
+    if (diff > 0) a[i] = a[i-1];
+    ans += diff;
+  }
+  cout << ans << endl;
+  return 0;
+}
