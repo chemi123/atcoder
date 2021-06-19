a89bb06a6a73d924771288035a18562900e4af4a
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-19 11:17:47 +0900

    one_quadrillion_and_one_dalmatians.cpp

---
+++ c_problems/one_quadrillion_and_one_dalmatians.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc171/tasks/abc171_c
+
+int main() {
+  ll n; cin >> n;
+  string ans("");
+  while (n > 0) {
+    --n;
+    int a = n % 26;
+    ans = (char)('a' + a) + ans;
+    n /= 26;
+  }
+  cout << ans << endl;
+  return 0;
+}
