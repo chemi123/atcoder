b889369c7aa89a9bfdc8bac9c8d852abd78ff42e
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-19 13:37:10 +0900

    multiplicatio_3.cpp

---
+++ c_problems/multiplicatio_3.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc169/submissions/23559347
+
+int main() {
+  long double a, b; cin >> a >> b;
+  cout << (long long)(a * b + 0.001) << endl;
+  return 0;
+}
