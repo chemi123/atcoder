14460efc9b954058b77c0be4af447989f8e04729
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-22 10:23:33 +0900

    imanagement.cpp

---
+++ c_problems/management.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc163/tasks/abc163_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n + 1);
+  rep (i, n - 1) {
+    int num; cin >> num;
+    ++a[num];
+  }
+  reps (i, 1, n + 1) cout << a[i] << endl;
+  return 0;
+}
