b16e6d09380ad317f81e0c2bfe98e814f64332b8
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-22 10:37:54 +0900

    replacing_integer.cpp

---
+++ c_problems/replacing_integer.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc161/tasks/abc161_c
+
+int main() {
+  ll n, k; cin >> n >> k;
+  ll d = n / k;
+  cout << min(abs(n - k * d), abs(n - k * d - k)) << endl;
+  return 0;
+}
