7686ddd89e04075ffc5809b3b316c6918718f370
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-23 22:28:13 +0900

    low_elements.cpp

---
+++ c_problems/low_elements.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc152/tasks/abc152_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> p(n);
+  rep (i, n) cin >> p[i];
+  int ans = 1, minNum = p[0];
+  reps (i, 1, n) {
+    if (p[i] <= minNum) ++ans;
+    minNum = min(minNum, p[i]);
+  }
+  cout << ans << endl;
+  return 0;
+}
