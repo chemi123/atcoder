a78ac2b890e8ad4d8d9f190da9d36a6a88bcf1f2
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-23 23:11:02 +0900

    snack.cpp

---
+++ c_problems/snack.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc148/tasks/abc148_c
+
+int main() {
+  ll a, b; cin >> a >> b;
+  cout << lcm(a, b) << endl;
+  return 0;
+}
