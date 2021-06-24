e83075db078eb3412783dc69f55c09cdc0a52f33
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-24 21:44:02 +0900

    slimes.cpp

---
+++ c_problems/slimes.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc143/tasks/abc143_c
+
+int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+  int ans = 1;
+  reps (i, 1, n) if (s[i] != s[i-1]) ++ans;
+  cout << ans << endl;
+  return 0;
+}
