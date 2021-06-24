7baffcfeb3749ff8607a25b688ebc7a3157d213b
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-24 21:44:11 +0900

    go_to_school.cpp

---
+++ c_problems/go_to_school.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc142/tasks/abc142_c
+
+int main() {
+  int n; cin >> n;
+  map<int, int> m;
+  rep (i, n) {
+    int num; cin >> num;
+    m[num] = i + 1;
+  }
+  for (auto [_, s] : m) cout << s << " "; cout << endl;
+  return 0;
+}
