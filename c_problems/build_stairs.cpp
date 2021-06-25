a9bb7a5be1b4c858adc983304de6af2be86ff1e8
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-25 09:03:01 +0900

    build_stairs.cpp

---
+++ c_problems/build_stairs.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc136/tasks/abc136_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> h(n);
+  rep (i, n) cin >> h[i];
+  bool ok = true;
+  for (int i = n - 1; i >= 1; --i) {
+    if (h[i-1] > h[i] + 1) {
+      ok = false;
+      break;
+    }
+    if (h[i-1] > h[i]) --h[i-1];
+  }
+
+  if (ok) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
