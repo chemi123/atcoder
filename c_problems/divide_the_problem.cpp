db3e2d3697b374c004b9fbf17a0c48145c09020f
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-06-26 13:02:03 +0900

    divide_the_problem.cpp

---
+++ c_problems/divide_the_problem.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc132/tasks/abc132_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> d(n);
+  rep (i, n) cin >> d[i];
+  sort(d.begin(), d.end());
+  cout << d[n/2] - d[(n/2)-1] << endl;
+  return 0;
+}
