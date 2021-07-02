b962b0055c0935e089c16fadf71e56d0522637e1
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-07-02 10:11:00 +0900

    five_transportations.cpp

---
+++ c_problems/five_transportations.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc123/tasks/abc123_c
+
+int main() {
+  ll n; cin >> n;
+  ll minNum = INFL;
+  rep (i, 5) {
+    ll num; cin >> num;
+    minNum = min(minNum, num);
+  }
+  cout << 4 + (n + minNum - 1) / minNum << endl;
+  return 0;
+}
