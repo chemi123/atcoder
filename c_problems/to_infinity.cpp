fa3c7b73400b5271c20d96cdac41f16e6ac8587b
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-07-07 08:52:00 +0900

    to_infinity.cpp

---
+++ c_problems/to_infinity.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc106/tasks/abc106_c
+
+int main() {
+  string s; cin >> s;
+  ll k; cin >> k;
+  int ans = 1;
+  for (; ans < s.size() && ans < k; ++ans) {
+    if (s[ans-1] != '1') break;
+  }
+  cout << s[ans-1] << endl;
+  return 0;
+}
