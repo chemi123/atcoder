112665278d120f3c726708a58077f040132d1988
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-07-10 10:12:02 +0900

    modulo_summation.cpp

---
+++ c_problems/modulo_summation.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc103/tasks/abc103_c
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  rep (i, n) cin >> a[i];
+  int sum = 0;
+  rep (i, n) sum += (a[i] - 1);
+  cout << sum << endl;
+  return 0;
+}
