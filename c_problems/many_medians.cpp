7486b0674c51dd5715e793a090fc83ea41cab6d8
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-07-17 14:58:36 +0900

    many_medians.cpp

---
+++ c_problems/many_medians.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc094/tasks/arc095_a
+
+int main() {
+  int n; cin >> n;
+  vector<int> x(n), sorted(n);
+  rep (i, n) {
+    cin >> x[i];
+    sorted[i] = x[i];
+  }
+  sort(sorted.begin(), sorted.end());
+  rep (i, n) {
+    if (x[i] >= sorted[n/2]) cout << sorted[n/2-1] << endl;
+    else cout << sorted[n/2] << endl;
+  }
+  return 0;
+}
