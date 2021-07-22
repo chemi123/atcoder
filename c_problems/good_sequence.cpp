bdb2b0df542e777f3f745d3da6c6d877f67867f6
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-07-22 09:26:52 +0900

    good_sequence.cpp

---
+++ c_problems/good_sequence.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc082/tasks/arc087_a
+
+int main() {
+  int n; cin >> n;
+  unordered_map<int, int> m;
+  rep (i, n) {
+    int a; cin >> a;
+    ++m[a];
+  }
+  ll ans = 0;
+  for (auto [e, num] : m) {
+    if (e > num) ans += num;
+    else if (e < num) ans += num - e;
+  }
+  cout << ans << endl;
+  return 0;
+}
