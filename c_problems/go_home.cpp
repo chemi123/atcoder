758626868d659c498cc6d2696834063bd9d6cdbe
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-08-08 09:04:00 +0900

    go_home.cpp

---
+++ c_problems/go_home.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc056/tasks/arc070_a
+
+int main() {
+  int x; cin >> x;
+  int sum = 0;
+  reps (i, 1, x + 1) {
+    sum += i;
+    if (sum >= x) {
+      cout << i << endl;
+      break;
+    }
+  }
+  return 0;
+}
