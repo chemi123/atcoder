3c8eff6f43159ff339779b7475d40fe3ee0c405b
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-08-08 10:10:25 +0900

    die_game.cpp

---
+++ c_problems/die_game.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc053/tasks/arc068_a
+
+int main() {
+  ll x; cin >> x;
+  ll ans = (x / 11) * 2;
+  if (x % 11 <= 6 && x % 11 != 0) ++ans;
+  else if (x % 11 >= 7) ans += 2;
+  cout << ans << endl;
+  return 0;
+}
