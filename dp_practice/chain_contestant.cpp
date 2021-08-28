7e4c7e3c73cf368c98e90f6a09c75d09554751e2
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-08-28 10:26:11 +0900

    chain_contestant.cpp

---
+++ dp_practice/chain_contestant.cpp
@@ -0,0 +1,122 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 998244353;
+
+template<int MOD> struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() const { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
+    is >> x.val;
+    x.val %= MOD;
+    if (x.val < 0) x.val += MOD;
+    return is;
+  }
+  friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(r, n / 2);
+    t = t * t;
+    if (n & 1) t = t * r;
+    return t;
+  }
+  friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    return Fp<MOD>(u);
+  }
+};
+
+using mint = Fp<MOD>;
+
+int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+  // dp[S][c]: i番目の文字を見ている時、使った文字の集合Sのうち最後に使った文字cが何通りあるか？
+  vector dp(1 << 10, vector<mint>(10));
+  for (auto c : s) {
+    int a = c - 'A';
+    vector tmp(1 << 10, vector<mint>(10));
+    swap(dp, tmp);
+    rep (S, 1 << 10) rep (i, 10) if (S >> i & 1) {
+      dp[S][i] += tmp[S][i];
+      if (i == a) dp[S][i] += tmp[S][i];
+      else if (~S >> a & 1) dp[S | 1 << a][a] += tmp[S][i];
+    }
+    dp[1 << a][a] += 1;
+  }
+  mint ans = 0;
+  rep (i, 1 << 10) rep (j, 10) ans += dp[i][j];
+  cout << ans << endl;
+  return 0;
+}
