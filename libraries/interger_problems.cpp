6e1286a4cf1fc344b61fdc52d15df2eb344dc66f
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-11-29 10:31:24 +0900

    fix

--- libraries/interger_problems.cpp
+++ libraries/interger_problems.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <vector>
 
 using ll = long long;
 
 using namespace std;
 
 // 素因数分解
-void primeFactorization(map<ll, ll>& m, ll n) {
+void primeFactorization(map<ll, ll>& s, ll n) {
   for (int i = 2; i * i <= n; ++i) {
     if (n % i != 0) continue;
     ll cnt = 0;
@@ -15,30 +16,45 @@ void primeFactorization(map<ll, ll>& m, ll n) {
       ++cnt;
       n /= i;
     }
-    m[i] = cnt;
+    s[i] = cnt;
   }
 
-  if (n != 1) m[n] = 1;
+  if (n != 1) s[n] = 1;
 }
 
 // 約数列挙
-void enumerateDivisor(set<ll>& m, ll n) {
+void enumerateDivisor(set<ll>& s, ll n) {
   for (int i = 1; i * i <= n; ++i) {
     if (n % i != 0) continue;
-    m.emplace(i);
-    if (i * i != n) m.emplace(n/i);
+    s.emplace(i);
+    if (i * i != n) s.emplace(n/i);
   }
 }
 
+// エラトステネスの篩
+void eratosthenes(vector<ll>& primes, ll n) {
+  vector<int> cnt(n + 1);
+  for (ll i = 2; i * i <= n; ++i) {
+    if (cnt[i] > 0) continue;
+    for (ll j = 2; i * j <= n; ++j) cnt[i * j] = 1;
+  }
+  for (int i = 2; i <= n; ++i) if (cnt[i] == 0) primes.emplace_back(i);
+}
+
 int main() {
-  map<ll, ll> m1;
+  map<ll, ll> s1;
   ll n = 200;
-  primeFactorization(m1, n);
-  for (auto [num, cnt] : m1) cout << num << ": " << cnt << endl; 
+  primeFactorization(s1, n);
+  for (auto [num, cnt] : s1) cout << num << ": " << cnt << endl; 
   cout << endl;
 
-  set<ll> m2;
-  enumerateDivisor(m2, n);
-  for (auto e : m2) cout << e << endl; 
+  set<ll> s2;
+  enumerateDivisor(s2, n);
+  for (auto e : s2) cout << e << endl; 
+  cout << endl;
+
+  vector<ll> v;
+  eratosthenes(v, 100);
+  for (auto e : v) cout << e << " "; cout << endl;
   return 0;
 }
