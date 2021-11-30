2bad0cc36ee3148100e73b7d46f881ed20f6e95b
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-11-30 10:13:43 +0900

    k_factors.cpp

---
+++ typical/k_factors.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/typical90/tasks/typical90_ad
+
+int main() {
+  int n, k; cin >> n >> k;
+  vector<int> cnt(n+1);
+  for (int i = 2; i <= n; ++i) {
+    if (cnt[i] > 0) continue;
+    for (int j = i; j <= n; j += i) ++cnt[j];
+  }
+  int ans = 0;
+  for (int i = 2; i <= n; ++i) if (cnt[i] >= k) ++ans;
+  cout << ans << endl;
+  return 0;
+}
