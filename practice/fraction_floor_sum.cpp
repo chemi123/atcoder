86839d62472f3c4ec5ac7dc9825235c4833a5ed3
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-12-04 16:41:36 +0900

    fraction_floor_sum.cpp

---
+++ practice/fraction_floor_sum.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc230/tasks/abc230_e
+
+int main() {
+  ll n; cin >> n;
+  ll ans = 0;
+  for (ll i = 1; i <= n;) {
+    ll x = n / i;
+    ll ni = n / x + 1;
+    ans += x * (ni - i);
+    i = ni;
+  }
+  cout << ans << endl;
+  return 0;
+}
