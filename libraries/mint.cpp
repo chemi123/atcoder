20e39033f5c58833ffe8127916e1c58a4805ef8f
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-12-18 09:21:31 +0900

    modify mint.cpp

--- libraries/mint.cpp
+++ libraries/mint.cpp
@@ -1,72 +1,24 @@
-template<int MOD> struct Fp {
-  long long val;
-  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
-    if (val < 0) val += MOD;
-  }
-  constexpr int getmod() const { return MOD; }
-  constexpr Fp operator - () const noexcept {
-    return val ? MOD - val : 0;
-  }
-  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
-  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
-  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
-  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
-  constexpr Fp& operator += (const Fp& r) noexcept {
-    val += r.val;
-    if (val >= MOD) val -= MOD;
-    return *this;
-  }
-  constexpr Fp& operator -= (const Fp& r) noexcept {
-    val -= r.val;
-    if (val < 0) val += MOD;
-    return *this;
-  }
-  constexpr Fp& operator *= (const Fp& r) noexcept {
-    val = val * r.val % MOD;
-    return *this;
-  }
-  constexpr Fp& operator /= (const Fp& r) noexcept {
-    long long a = r.val, b = MOD, u = 1, v = 0;
-    while (b) {
-      long long t = a / b;
-      a -= t * b, swap(a, b);
-      u -= t * v, swap(u, v);
-    }
-    val = val * u % MOD;
-    if (val < 0) val += MOD;
-    return *this;
-  }
-  constexpr bool operator == (const Fp& r) const noexcept {
-    return this->val == r.val;
-  }
-  constexpr bool operator != (const Fp& r) const noexcept {
-    return this->val != r.val;
-  }
-  friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
-    is >> x.val;
-    x.val %= MOD;
-    if (x.val < 0) x.val += MOD;
-    return is;
-  }
-  friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
-    return os << x.val;
-  }
-  friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
-    if (n == 0) return 1;
-    auto t = modpow(r, n / 2);
-    t = t * t;
-    if (n & 1) t = t * r;
-    return t;
-  }
-  friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
-    long long a = r.val, b = MOD, u = 1, v = 0;
-    while (b) {
-      long long t = a / b;
-      a -= t * b, swap(a, b);
-      u -= t * v, swap(u, v);
-    }
-    return Fp<MOD>(u);
-  }
+template<int MOD> struct ModInt {
+    static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+    ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    int get() const { return (int)x; }
+    ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+    ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+    ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+    ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return ModInt(u); }
+    bool operator==(ModInt that) const { return x == that.x; }
+    bool operator!=(ModInt that) const { return x != that.x; }
+    ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
 };
-
-// using mint = Fp<MOD>;
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+    ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<1000000007> mint;
