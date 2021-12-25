df3246cd4892a69c8468fcdd0cbdcf8f38f571c6
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-12-25 10:11:16 +0900

    powerful_discount_tickets2.cpp

---
+++ practice/powerful_discount_tickets2.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// https://atcoder.jp/contests/abc141/tasks/abc141_d
+
+int main() {
+  ll n, m; cin >> n >> m;
+  priority_queue<ll> pq;
+  rep (i, n) {
+    ll a; cin >> a;
+    pq.emplace(a);
+  }
+  rep (i, m) {
+    pq.emplace(pq.top()/2);
+    pq.pop();
+  }
+  ll ans = 0;
+  while (!pq.empty()) {
+    ans += pq.top();
+    pq.pop();
+  }
+  cout << ans << endl;
+  return 0;
+}
