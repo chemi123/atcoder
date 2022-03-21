13011e3ade34e843704f2bb5af935b4e60319ac3
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2022-03-21 20:29:51 +0900

    king_bombee.cpp

---
+++ dp/king_bombee.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+template<int MOD> struct ModInt {
+    static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+    ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    int get() const { return (int)x; }
+    ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+    ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+    ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+    ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return ModInt(u); }
+    bool operator==(ModInt that) const { return x == that.x; }
+    bool operator!=(ModInt that) const { return x != that.x; }
+    ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+    ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+// https://atcoder.jp/contests/abc244/tasks/abc244_e
+
+int main() {
+  int n, m, k, s, t, x; cin >> n >> m >> k >> s >> t >> x; --s, --t, --x;
+  vector<vector<int>> graph(n);
+  rep (i, m) {
+    int u, v; cin >> u >> v; --u, --v;
+    graph[u].emplace_back(v);
+    graph[v].emplace_back(u);
+  }
+  vector dp(k + 1, vector(n, vector<mint>(2)));
+  dp[0][s][0] = 1;
+  reps (i, 1, k + 1) {
+    rep (j, n) {
+      for (auto from : graph[j]) {
+        if (j == x) {
+          dp[i][j][0] += dp[i-1][from][1];
+          dp[i][j][1] += dp[i-1][from][0];
+        } else {
+          dp[i][j][0] += dp[i-1][from][0];
+          dp[i][j][1] += dp[i-1][from][1];
+        }
+      }
+    }
+  }
+  cout << dp.back()[t][0] << endl;
+  return 0;
+}
