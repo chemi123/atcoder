6332ae8276a35e859d26120bc8dca72187c945a9
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2022-04-02 13:54:22 +0900

    between_two_arrays.cpp

--- dp/between_two_arrays.cpp
+++ dp/between_two_arrays.cpp
@@ -24,34 +24,52 @@ using pl = pair<ll, ll>;
 
 const int INF = 1e9;
 const ll INFL = 1e18;
-const ll MOD = 998244353;
+const ll MOD = 1000000007;
+
+template<int MOD> struct ModInt {
+    static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+    ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    int get() const { return (int)x; }
+    ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+    ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+    ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+    ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return ModInt(u); }
+    bool operator==(ModInt that) const { return x == that.x; }
+    bool operator!=(ModInt that) const { return x != that.x; }
+    ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+    ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
 
 // https://atcoder.jp/contests/abc222/tasks/abc222_d
 
 int main() {
   int n; cin >> n;
-  vector<ll> a(n), b(n);
+  vector<int> a(n), b(n);
   rep (i, n) cin >> a[i];
   rep (i, n) cin >> b[i];
-  
-  int maxNum = 3000;
-  vector dp(n, vector<ll>(maxNum + 1));
-  if (a[0] == 0) dp[0][0] = 1;
-  reps (i, 1, maxNum + 1) {
-    if (a[0] <= i && i <= b[0]) dp[0][i] = dp[0][i-1] + 1;
-    else dp[0][i] = dp[0][i-1];
-  }
-
+  int maxNum = max(a.back(), b.back());
+  vector<mint> dp(maxNum + 1);
+  for (int i = a[0]; i <= b[0]; ++i) dp[i] = 1;
   reps (i, 1, n) {
-    reps (j, a[i], maxNum + 1) {
-      if (j == 0) {
-        dp[i][j] = 1;
-        continue;
-      }
-      if (a[i] <= j && j <= b[i]) dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
-      else dp[i][j] = dp[i][j-1];
-    }
+    vector<mint> acc(maxNum + 1), dp2(maxNum + 1);
+    acc[0] = dp[0];
+    reps (j, 1, maxNum + 1) acc[j] = acc[j-1] + dp[j];
+    for (int j = a[i]; j <= b[i]; ++j) dp2[j] = acc[j];
+    swap(dp, dp2);
   }
-  cout << dp.back().back() << endl;
+  mint ans = 0;
+  for (auto e : dp) ans += e;
+  cout << ans << endl; 
   return 0;
 }
