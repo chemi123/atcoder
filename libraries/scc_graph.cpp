5e8a2cdff58838189cdbbeba35009ec135cd323f
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2022-04-10 10:03:10 +0900

    fix

--- libraries/scc_graph.cpp
+++ libraries/scc_graph.cpp
@@ -59,61 +59,61 @@ struct scc_graph {
 
     // @return pair of (# of scc, scc id)
     std::pair<int, std::vector<int>> scc_ids() {
-        auto g = csr<edge>(_n, edges);
-        int now_ord = 0, group_num = 0;
-        std::vector<int> visited, low(_n), ord(_n, -1), ids(_n);
-        visited.reserve(_n);
-        auto dfs = [&](auto self, int v) -> void {
-            low[v] = ord[v] = now_ord++;
-            visited.push_back(v);
-            for (int i = g.start[v]; i < g.start[v + 1]; i++) {
-                auto to = g.elist[i].to;
-                if (ord[to] == -1) {
-                    self(self, to);
-                    low[v] = std::min(low[v], low[to]);
-                } else {
-                    low[v] = std::min(low[v], ord[to]);
-                }
-            }
-            if (low[v] == ord[v]) {
-                while (true) {
-                    int u = visited.back();
-                    visited.pop_back();
-                    ord[u] = _n;
-                    ids[u] = group_num;
-                    if (u == v) break;
-                }
-                group_num++;
-            }
-        };
-        for (int i = 0; i < _n; i++) {
-            if (ord[i] == -1) dfs(dfs, i);
+      auto g = csr<edge>(_n, edges);
+      int now_ord = 0, group_num = 0;
+      std::vector<int> visited, low(_n), ord(_n, -1), ids(_n);
+      visited.reserve(_n);
+      auto dfs = [&](auto self, int v) -> void {
+        low[v] = ord[v] = now_ord++;
+        visited.push_back(v);
+        for (int i = g.start[v]; i < g.start[v + 1]; i++) {
+          auto to = g.elist[i].to;
+          if (ord[to] == -1) {
+            self(self, to);
+            low[v] = std::min(low[v], low[to]);
+          } else {
+            low[v] = std::min(low[v], ord[to]);
+          }
         }
-        for (auto& x : ids) {
-            x = group_num - 1 - x;
+        if (low[v] == ord[v]) {
+          while (true) {
+            int u = visited.back();
+            visited.pop_back();
+            ord[u] = _n;
+            ids[u] = group_num;
+            if (u == v) break;
+          }
+          group_num++;
         }
-        return {group_num, ids};
+      };
+      for (int i = 0; i < _n; i++) {
+        if (ord[i] == -1) dfs(dfs, i);
+      }
+      for (auto& x : ids) {
+        x = group_num - 1 - x;
+      }
+      return {group_num, ids};
     }
 
     std::vector<std::vector<int>> scc() {
-        auto ids = scc_ids();
-        int group_num = ids.first;
-        std::vector<int> counts(group_num);
-        for (auto x : ids.second) counts[x]++;
-        std::vector<std::vector<int>> groups(ids.first);
-        for (int i = 0; i < group_num; i++) {
-            groups[i].reserve(counts[i]);
-        }
-        for (int i = 0; i < _n; i++) {
-            groups[ids.second[i]].push_back(i);
-        }
-        return groups;
+      auto ids = scc_ids();
+      int group_num = ids.first;
+      std::vector<int> counts(group_num);
+      for (auto x : ids.second) counts[x]++;
+      std::vector<std::vector<int>> groups(ids.first);
+      for (int i = 0; i < group_num; i++) {
+        groups[i].reserve(counts[i]);
+      }
+      for (int i = 0; i < _n; i++) {
+        groups[ids.second[i]].push_back(i);
+      }
+      return groups;
     }
 
   private:
     int _n;
     struct edge {
-        int to;
+      int to;
     };
     std::vector<std::pair<int, edge>> edges;
 };
@@ -124,10 +124,10 @@ struct scc_graph {
     explicit scc_graph(int n) : internal(n) {}
 
     void add_edge(int from, int to) {
-        int n = internal.num_vertices();
-        assert(0 <= from && from < n);
-        assert(0 <= to && to < n);
-        internal.add_edge(from, to);
+      int n = internal.num_vertices();
+      assert(0 <= from && from < n);
+      assert(0 <= to && to < n);
+      internal.add_edge(from, to);
     }
 
     std::vector<std::vector<int>> scc() { return internal.scc(); }
