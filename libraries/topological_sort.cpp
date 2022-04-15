4db44b7e014e14a03b6516f2236802fa78599325
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2022-04-15 10:37:03 +0900

    topological_sort.cpp

---
+++ libraries/topological_sort.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define reps(i, s, n) for (int i = s; i < n; i++)
+#define debug(s, param) std::cerr << s << param << std::endl;
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const ll MOD = 1000000007;
+
+// 0indexのグラフでで使用すること
+class TopologicalSort {
+public:
+  TopologicalSort(int n) : _graph(vector<vector<int>>(n)), _n(n) {}
+  
+  void add_edge(int a, int b) { _graph[a].emplace_back(b); }
+
+  vector<int> topologicalSort() {
+    vector<bool> visit(_n);
+    vector<int> res;
+    res.reserve(_n);
+    for (int i = 0; i < _n; ++i) dfs(res, visit, i);
+    reverse(res.begin(), res.end());
+    return res;
+  }
+
+private:
+  void dfs(vector<int>& v, vector<bool>& visit, int current) {
+    if (visit[current]) return;
+    visit[current] = true;
+    for (auto next : _graph[current]) dfs(v, visit, next);
+    v.emplace_back(current);
+  }
+
+  int _n;
+  vector<vector<int>> _graph;
+};
+
+// 以下で動作確認できている
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B
+
+int main() {
+  int v, e; cin >> v >> e;
+  TopologicalSort ts(v);
+  rep (i, e) {
+    int s, t; cin >> s >> t;
+    ts.add_edge(s, t);
+  }
+  vector<int> ans = ts.topologicalSort();
+  for (auto e : ans) cout << e << endl;
+  return 0;
+}
